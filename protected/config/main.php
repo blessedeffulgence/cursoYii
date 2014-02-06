f11a9a0712720c51dc02531d68cfb667b1b2defa
comunas <comunas@debian>
2014-02-06 17:35:56 -0430

    Se modifico el nombre del proyecto en mi archivo config/main.php

--- protected/config/main.php
+++ protected/config/main.php
@@ -7,7 +7,7 @@
 // CWebApplication properties can be configured here.
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'Curso Yii',
+	'name'=>'Curso de Yii Framework',
 
 	// preloading 'log' component
 	'preload'=>array(
